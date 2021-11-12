96fe1ec4f5c661f0f691ba72fb720ab069522fcb
@zhaogy <m17681891251@163.com>
2021-11-12 17:27:26 +0800

    login和register antd改造

--- my-app/src/unauthenticated/login.tsx
+++ my-app/src/unauthenticated/login.tsx
@@ -1,32 +1,37 @@
 // PAKAGE
+import { Form, Input, Button } from 'antd';
 import { useAuth } from "context/auth-context";
 
 // FUNCTION JSX
 export const LoginScreen = () => {
   // FUNCTION DIFINED
-  let handlSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-
+  // ############## let handlSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  let handlSubmit: (values: { username: string, password: string }) => void;
   const { login, user } = useAuth();
-  handlSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+
+  // 原生
+  // ############## handlSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  // ##############   event.preventDefault()
+  // ##############   const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+  // ##############   const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+  // ##############   login({ username, password })
+  // ############## }
+
+  // antd 提交事件
+  handlSubmit = ({ username, password }) => {
     login({ username, password })
   }
-  // JSX
   return (
-    <form onSubmit={handlSubmit}>
-      <div>
-        <label htmlFor="username">
-          用户名
-        </label>
-        <input type="text" id={"username"}></input>
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="text" id={"password"} />
-      </div>
-      <button type={"submit"}>登录</button>
-    </form>
+    <Form onFinish={handlSubmit}>
+      <Form.Item name={'username'} rules={[{ required: true, message: '请输入用户名' }]}>
+        <Input placeholder={'username'} type="text" id={"username"}></Input>
+      </Form.Item>
+      <Form.Item name={'password'} rules={[{ required: true, message: '请输入密码' }]}>
+        <Input placeholder={'password'} type="text" id={"password"} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType={"submit"} type={'primary'}>Login</Button>
+      </Form.Item>
+    </Form>
   );
 };
--- my-app/src/unauthenticated/register.tsx
+++ my-app/src/unauthenticated/register.tsx
@@ -1,32 +1,34 @@
 // PAKAGE
+import { Form, Input, Button } from 'antd';
 import { useAuth } from "context/auth-context";
 
 // FUNCTION JSX
 export const RegisterScreen = () => {
-  // FUNCTION DIFINED
-  let handlSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  // ###############let handlSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  let handlSubmit: (values: { username: string, password: string }) => void;
+  const { register } = useAuth();
 
-  const { login, user } = useAuth();
-  handlSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
-    const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
-    login({ username, password })
+  // ############### handlSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  // ###############   event.preventDefault()
+  // ###############   const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+  // ###############   const password = (event.currentTarget.elements[1] as HTMLInputElement).value;
+  // ###############   login({ username, password })
+  // ############### }
+
+  handlSubmit = ({ username, password }) => {
+    register({ username, password })
   }
-  // JSX
   return (
-    <form onSubmit={handlSubmit}>
-      <div>
-        <label htmlFor="username">
-          用户名
-        </label>
-        <input type="text" id={"username"}></input>
-      </div>
-      <div>
-        <label htmlFor="password">密码</label>
-        <input type="text" id={"password"} />
-      </div>
-      <button type={"submit"}>注册</button>
-    </form>
+    <Form onFinish={handlSubmit}>
+      <Form.Item name={'username'}>
+        <Input placeholder={'username'} type="text" id={"username"}></Input>
+      </Form.Item>
+      <Form.Item name={'password'}>
+        <Input type="text" id={"password"} />
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType={'submit'} type={'primary'}>注册</Button>
+      </Form.Item>
+    </Form>
   );
 };
